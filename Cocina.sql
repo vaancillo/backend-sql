a10a7c7a21f4316bbb137d1bf56c3c62491ded0a
vancillo <yaredbane@gmail.com>
2023-10-22 12:28:12 -0600

    first commit

---
+++ Cocina.sql
@@ -0,0 +1,11 @@
+/*
+ * Una cocina de elite quiere mejorar su sistema
+ * de empleados ya que actualmente su base de datos
+ * consta unicamente de una tabla llamada empleados
+ * ellos quieren tener dos mas, cocinero y mesero.
+ * Tambien quieren saber que meseros pertenecen
+ * a que cocinero, cumpliendo la condicion
+ * donde un mesero tiene un solo cocinero,
+ * pero los cocineros tienen varios meseros
+ * 
+ * */
